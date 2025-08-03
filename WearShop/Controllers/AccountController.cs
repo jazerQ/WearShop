ad9c1f5dbb337f4eaab54affc72bc8d0e09c88c5
Jazer <saltanhayrliev2006@gmail.com>
2025-08-03 21:40:48 +0300

    добавил пустой layout для страницы регистрации

--- WearShop/Controllers/AccountController.cs
+++ WearShop/Controllers/AccountController.cs
@@ -1,8 +1,17 @@
+using DataAccess.Entities.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using WearShop.Controllers.ExtendedControllers;
 
 namespace WearShop.Controllers;
 
-public class AccountController : ExtensionController
+public class AccountController(
+    UserManager<User> _userManager,
+    SignInManager<User> _signInManager) : ExtensionController
 {
-    
+    [HttpGet("/[controller]/register")]
+    public IActionResult Register()
+    {
+        return View("Register");
+    }
 }
